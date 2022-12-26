69d741f0132336f0fe1c55377a46d0e4a6e085a3
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2022-12-26 11:57:28 -0500

    Update Lab2.cpp

--- Lab2.cpp
+++ Lab2.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 int main(){
+    // One acre of land is equivalent to 43,560 square feet. 
+    // Write a program that prompts the user to enter the total square footage 
+    // in a tract of land, calculates, and prints the result of the conversion 
+    // from square feet to acres. Create a constant named ACRE_FACTOR to express 
+    // the conversion factor. Format the output to limit the number of digits after 
+    // the decimal point to two digits.
     // Place your solution code here for Lab2
+    
+    
 
     return 0;
 }
