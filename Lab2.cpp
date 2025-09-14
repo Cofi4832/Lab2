7b70f9ac5c8808235b318b48cfb0b9b5ee3ced0c
Cofi4832 <dalex@umich.edu>
2025-09-13 23:57:28 -0400

    Attempt 1

--- Lab2.cpp
+++ Lab2.cpp
@@ -6,5 +6,7 @@ int main(){
     double acres; // holds the conversion result of square feet to acres
     // Place your solution code here for Lab2
 
+    //testing push and commit
+
     return 0;
 }
